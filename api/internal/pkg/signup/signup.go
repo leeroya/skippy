aa8791f9d2535e8aa3f940fe2f20f5f76f2d717d
Lee-Roy Ashworth <leeroya@gmail.com>
2020-04-14 06:50:56 +0200

    sign up changes

--- api/internal/pkg/signup/signup.go
+++ api/internal/pkg/signup/signup.go
@@ -1,1 +1,26 @@
-package signup
+package signUp
+
+import . "../../../config"
+
+var ValidUser string
+
+var ValidPassword string
+
+func Register(username string, password string, configurations Configurations) Model {
+	if len(username) < 1 {
+		return Model{Code: 400, Condition: false, Message: "Unsatisfactory input variables"}
+	}
+	if len(password) < 1 {
+		return Model{Code: 400, Condition: false, Message: "Unsatisfactory input variables"}
+	}
+
+	if username != ValidUser {
+		return Model{Code: 400, Condition: false, Message: "Unsatisfactory input variables"}
+	}
+
+	if password != ValidPassword {
+		return Model{Code: 400, Condition: false, Message: "Unsatisfactory input variables"}
+	}
+
+	return Model{Code: 200, Condition: true, Message: "Success"}
+}
